79ced20954bdabbad8b8a1795f38834ccef1dcc2
Flemming Frandsen <dren.dk@gmail.com>
2013-06-11 21:53:59 +0200

    Started messing about with the AVR PWM

--- firmware/ledstrip.c
+++ firmware/ledstrip.c
@@ -47,6 +47,8 @@ void led(char on) {
 }
 
 
+
+
 int main(void) {
   wdt_enable(WDTO_4S);
   DDRD  |= _BV(PD4);  // LED output
@@ -63,12 +65,25 @@ int main(void) {
 
   mprintf(PSTR("Power up\n"));
 
-  DDRB  |= _BV(PB1);  // OC1A
-  //DDRB  |= _BV(PB3);  // OC2A
+  DDRD  |= _BV(PD3);  // OC2B
+
+  // Set up timer 0 for fast PWM mode & the highest frequency available
+  TCCR0A = _BV(COM0A1) | _BV(COM0B1) | _BV(COM0B0) |  _BV(WGM00) |  _BV(WGM01);
+  TCCR0B = _BV(CS00);
+  DDRD  |= _BV(PD6);  // OC0A
+  DDRD  |= _BV(PD5);  // OC0A
 
   // Set up timer 1 for fast PWM mode & the highest frequency available
-  TCCR1A = _BV(WGM10);
+  TCCR1A = _BV(COM1A1) | _BV(COM1B1) | _BV(COM1B0) | _BV(WGM10);
   TCCR1B = _BV(WGM12) | _BV(CS10);
+  DDRB  |= _BV(PB1);  // OC1A
+  DDRB  |= _BV(PB2);  // OC1A
+
+  OCR0A = 50;
+  OCR0B = 254; // White
+  OCR1A = 0;
+  OCR1B = 0;
+
 
   i2c_init();
   pca9685_init(0x80, PCA9685_FREQUENCY(1200UL));
@@ -84,6 +99,5 @@ int main(void) {
     pca9685_led_pwm(0x80, 0, frame);
     pca9685_led_pwm(0x80, 1, 128-frame);
     pca9685_led_pwm(0x80, 2, 255-frame);
-
   }
 }
